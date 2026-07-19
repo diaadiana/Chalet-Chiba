9b4b65267b050ffceeee28212e7c5ac465f256a0
diaadiana <diaadiana@users.noreply.github.com>
2026-07-19 17:11:41 +0300

    Implement SEO and Core Web Vitals optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://chaletchiba.ro',
+  integrations: [
+    react(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.9,
+      lastmod: new Date(),
+    })
+  ]
 });
